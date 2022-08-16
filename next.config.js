4477766486a8c434d293bc258443b6bd5a943446
AP <ap@APs-MacBook-Pro.local>
2022-08-16 12:37:12 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
 module.exports = nextConfig
+
+// config walling
+
+// module.exports = {
+//   webpack(config) {
+//     config.infrastructureLogging = { debug: /PackFileCache/ }
+//     return config;
+//   }
+// }
